c4c09d1b313d4ecfd25a8c9b7db5afb2fbb81d2c
andreas <andr493i@elev.techcollege.dk>
2024-11-19 09:08:39 +0100

    index

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+// index.js
+import express from 'express';
+import cors from 'cors';
+import router from './sangbog/router.js';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
 
 
- app.listen(port, () => {
-   console.log(`server kører på http://localhost:${port}`);
- });
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
